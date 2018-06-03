60447d563ee6da428c2c5ac146a314789e438883
nodaguti <nodaguti@gmail.com>
2018-06-04 00:23:16 +0900

    Use the latest Java

--- zsh/java.sh
+++ zsh/java.sh
@@ -1,2 +1,2 @@
-export JAVA_HOME=`/System/Library/Frameworks/JavaVM.framework/Versions/A/Commands/java_home -v "9"`
+export JAVA_HOME=`/usr/libexec/java_home`
 export PATH="${JAVA_HOME}/bin:${PATH}"
