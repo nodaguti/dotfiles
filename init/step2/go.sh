a23c80f0b09cd76878f04d72a157a2f4c372dcb2
nodaguti <nodaguti@gmail.com>
2018-06-07 09:28:03 +0900

    Install all programming packages with Brewfile

--- init/step2/go.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-brew install go
-brew install dep
-
-export GOPATH="${HOME}/go"
-export PATH="$PATH:$GOPATH/bin"
--- init/step2/node.sh
+++ init/step2/node.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# Install version manager
-brew install nodenv
+# Initialise the version manager
 nodenv init
 
 # Install the latest version of Node
@@ -9,8 +8,4 @@ LATEST_NODE=`nodenv install -l | tr -d ' ' | grep -E '^[0-9.]+$' | sort -t. -k 1
 nodenv install $LATEST_NODE
 
 # Install nodenv's plugins
-brew install nodenv/nodenv/nodenv-npm-migrate
 git clone https://github.com/nodenv/nodenv-package-rehash.git "$(nodenv root)"/plugins/nodenv-package-rehash
-
-# Install yarn
-brew install yarn --without-node
