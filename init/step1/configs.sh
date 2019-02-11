60eb81faae7f18ecaf4fb213c7e3000d247d3aad
nodaguti <nodaguti@gmail.com>
2019-02-11 12:01:55 +0900

    Create a folder in which screenshots are saved in advance

--- init/step1/configs.sh
+++ init/step1/configs.sh
@@ -252,6 +252,7 @@ defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 # Save screenshots to the desktop/screenshots
+mkdir "${HOME}/Desktop/screenshots"
 defaults write com.apple.screencapture location -string "${HOME}/Desktop/screenshots"
 
 # Disable shadow in screenshots
