e5d7aa68306ff76f7dde93b2eec9cc0afc7ae9fc
nodaguti <nodaguti@gmail.com>
2019-02-11 10:51:53 +0900

    Fix an issue where zsh was not appended to /etc/shells

--- init/step1/zsh.sh
+++ init/step1/zsh.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 brew install zsh zsh-completions
-sudo echo "/usr/local/bin/zsh" >> /etc/shells
+sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
 chsh -s /usr/local/bin/zsh
 
 ln -s "${HOME}/dotfiles/zsh/.zshrc" "${HOME}/.zshrc"
