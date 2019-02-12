2160317389aa7c410ee57c5f836533cbab1ad7f6
nodaguti <nodaguti@gmail.com>
2019-02-12 21:13:09 +0900

    Fix a path to zshrc

--- init/step2/zsh.sh
+++ init/step2/zsh.sh
@@ -4,7 +4,7 @@ brew install zsh zsh-completions
 sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
 chsh -s /usr/local/bin/zsh
 
-ln -s "${HOME}/dotfiles/zsh/.zshrc" "${HOME}/.zshrc"
+ln -s "${HOME}/workspaces/dotfiles/zsh/.zshrc" "${HOME}/.zshrc"
 
 # zgen
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
