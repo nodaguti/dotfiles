f8d7d197aacb488a66c633051ca5cf4e39ec153d
nodaguti <nodaguti@gmail.com>
2021-01-07 12:56:37 +0900

    Update installation command of Homebrew

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install git
 brew install git
