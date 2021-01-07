4b0c24228c85203e96bb2d168a7eb55025ff4b1a
nodaguti <nodaguti@gmail.com>
2021-01-07 20:40:46 +0900

    Fix that zgen didn't load completions provided by homebrew

--- init/step2/zsh.sh
+++ init/step2/zsh.sh
@@ -2,9 +2,10 @@
 
 brew install zsh zsh-completions
 sudo sh -c 'echo $(brew --prefix)/bin/zsh >> /etc/shells'
-chsh -s /usr/local/bin/zsh
+chsh -s $(brew --prefix)/bin/zsh
 
 ln -s "${HOME}/workspaces/dotfiles/zsh/.zshrc" "${HOME}/.zshrc"
 
 # zgen
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+compaudit | xargs chmod g-w,o-w
