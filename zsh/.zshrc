dc43b46c80a3569cb8d3957fe77903aef855d512
nodaguti <nodaguti@gmail.com>
2021-01-07 20:06:45 +0900

    Fix that coreutils weren't loaded

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -10,12 +10,6 @@ export LC_ALL=ja_JP.UTF-8
 ###
 # PATH
 ###
-export PATH="/usr/local/bin:$PATH"
-export PATH="/usr/local/sbin:$PATH"
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
-
-# Package-specific paths
 source "$HOME/workspaces/dotfiles/zsh/homebrew.sh"
 source "$HOME/workspaces/dotfiles/zsh/paths.sh"
 
--- zsh/paths.sh
+++ zsh/paths.sh
@@ -1,13 +1,21 @@
+# Core Utils
+export PATH="$(brew --prefix)/opt/coreutils/libexec/gnubin:$PATH"
+export MANPATH="$(brew --prefix)/opt/coreutils/libexec/gnuman:$MANPATH"
+
+# Java
 export JAVA_HOME=`/usr/libexec/java_home`
 export PATH="${JAVA_HOME}/bin:${PATH}"
 
+# Python
 export PATH="${HOME}/.poetry/bin:${PATH}"
 
+# Go
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 if [ ! -e $GOPATH ]; then
   mkdir -p $GOPATH
 fi
 
+# Google Cloud SDK
 if [ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]; then source "$HOME/google-cloud-sdk/path.zsh.inc"; fi
 if [ -f "$HOME/google-cloud-sdk/completion.zsh.inc" ]; then source "$HOME/google-cloud-sdk/completion.zsh.inc"; fi
