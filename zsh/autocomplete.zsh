47f20132481e8949dfed301610f28e15b075f539
nodaguti <nodaguti@gmail.com>
2021-12-28 16:25:33 +0900

    Add homebrewed packages to autocomplete candidates on arm-based Mac

--- zsh/autocomplete.zsh
+++ zsh/autocomplete.zsh
@@ -33,4 +33,4 @@ zstyle ':completion:*' ignore-parents parent pwd ..
 
 # Auto-complete after `sudo`
 zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin \
-/usr/sbin /usr/bin /sbin /bin /usr/X11R6/bin
+/usr/sbin /usr/bin /sbin /bin /usr/X11R6/bin /opt/homebrew/sbin /opt/homebrew/bin
