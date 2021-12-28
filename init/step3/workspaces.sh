2d3877980d2c521e11ee71a04c5de63338851e4e
nodaguti <nodaguti@gmail.com>
2021-12-28 17:22:44 +0900

    Use gh command to clone all my repositories into workspace

--- init/step3/workspaces.sh
+++ init/step3/workspaces.sh
@@ -8,22 +8,6 @@ fi
 
 pushd "${HOME}/workspaces"
 
-REPOS_URL=`curl -s "https://api.github.com/users/${USERNAME}" | jq -r .repos_url`
-PAGE=1
-
-while :
-do
-  REPOS=`curl -s "${REPOS_URL}?page=${PAGE}"`
-
-  if [ `echo $REPOS | jq '. | length'` -eq 0 ]; then
-    break
-  fi
-
-  echo $REPOS \
-  | jq -r '.[] | select(.fork == false) | select(.name != "dotfiles") | .ssh_url' \
-  | xargs -n1 git clone
-
-  PAGE=`expr ${PAGE} + 1`
-done
+gh repo list ${USERNAME} --limit 100 --source | sed -e 's/\t.*$//g' | xargs -n 1 gh repo clone
 
 popd
